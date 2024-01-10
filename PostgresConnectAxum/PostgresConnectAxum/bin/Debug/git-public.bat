88780a6ad3f14428947649337952820d7ddb7b3d
Willem65 <Willempost77@gmail.com>
2024-01-11 00:56:38 +0100

    Commit push message

--- PostgresConnectAxum/PostgresConnectAxum/bin/Debug/git-public.bat
+++ PostgresConnectAxum/PostgresConnectAxum/bin/Debug/git-public.bat
@@ -1,24 +1,18 @@
-
 @echo off
-@cd %1
-@echo.
-@echo Make Repository on GitHub.
-@echo.
-@echo This is the :   git-repo.bat    file
-@echo.
-
-
+echo.
+echo Dit is de:  git-private.bat  
+echo.
+rem @cd %1
 set sLongString=%1
 rem set sLongString="C:\Users\willem\Documents\Visual Studio 2017\Projects\PostgresConnectAxum"
 SET sSubStr="%sLongString:~3%
-rem echo %sLongString%
+echo %sLongString%
 REM echo %sSubStr%
 for %%F in ("%sSubStr%") do set foldername=%%~nxF
 
 set foldername=%foldername:"=%
-@echo.
 echo %foldername%
-@echo.
 gh.exe repo create %foldername% --public --source=%sLongString%  --remote=upstream --push
+rem gh.exe repo create %foldername% --private --source="C:\Users\willem\Documents\Visual Studio 2017\Projects\PostgresConnectAxum" --remote=upstream --push
 
 REM SET sSubStr="\Users\willem\Documents\Visual Studio 2017\Projects\PostgresConnectAxum"
